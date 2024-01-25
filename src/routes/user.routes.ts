18989d449ad27ffeabe67b996dfaba9728f80474
Vinicius Kalach <viniciuscato@hotmail.com>
2024-01-24 23:43:23 -0300

    Create login user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreaterUserController } from "../modules/users/useCases/createUser/CreateUserController";
+import { LoginUserController } from "../modules/users/useCases/loginUser/LoginUserController";
 
 const createrUserController = new CreaterUserController();
+const loginUserController = new LoginUserController();
 
 const userRoutes = Router();
 
-userRoutes.post("/", createrUserController.handle);
+userRoutes.post("/create", createrUserController.handle);
+userRoutes.get("/login", loginUserController.handle)
 
 export { userRoutes };
