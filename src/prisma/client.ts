aa140040dddca2543205f767fc4d821b6b067af2
Vinicius Kalach <viniciuscato@hotmail.com>
2024-01-26 23:14:30 -0300

    refactor: client ts

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,3 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient
+export const prisma = new PrismaClient ({ datasources: { db: { url: "mysql://kalach:sjvm%402003@netflexdb.mysql.uhserver.com:3306/netflexdb"}}});
